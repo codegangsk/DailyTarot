d5afa073c1a6ebaa2e113b807c29c0e684f6f775
codegangsk <kjhj0604@gmail.com>
2021-08-30 14:19:46 +0900

    add models

---
+++ DailyTarot/Application/Models/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  DailyTarot
+//
+//  Created by Sophie Kim on 2021/08/30.
+//
+
+import Foundation
+
+struct Card: Codable {
+    var type: String?
+    var name_short: String?
+    var value: String?
+}
+
+
---
+++ DailyTarot/Application/Models/Cards.swift
@@ -0,0 +1,13 @@
+//
+//  Cards.swift
+//  DailyTarot
+//
+//  Created by Sophie Kim on 2021/08/30.
+//
+
+import Foundation
+
+struct Cards: Codable {
+    var cards: [Card]?
+}
+
